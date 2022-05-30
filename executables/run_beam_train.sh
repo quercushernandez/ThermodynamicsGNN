47d3faae185b70baaa7a8a26422a7acc30acb09d
Quercus <63294776+quercushernandez@users.noreply.github.com>
2022-05-30 17:00:45 +0200

    Update run_beam_train.sh

--- executables/run_beam_train.sh
+++ executables/run_beam_train.sh
@@ -2,5 +2,4 @@
 python main.py --sys_name beam --train True \
     --n_hidden 2 --dim_hidden 50 --passes 10 \
     --miles 600 1200 --gamma 0.1 --max_epoch 1800 \
-    --noise_var 1e-5 --lr 1e-4 --lambda_d 50 \
-    --gpu False
+    --noise_var 1e-5 --lr 1e-4 --lambda_d 50
