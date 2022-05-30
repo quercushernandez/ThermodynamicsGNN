0e079e330e49c3947376e84c38704c416370129c
Quercus <63294776+quercushernandez@users.noreply.github.com>
2022-05-30 17:01:20 +0200

    Update run_cylinder_train.sh

--- executables/run_cylinder_train.sh
+++ executables/run_cylinder_train.sh
@@ -2,5 +2,4 @@
 python main.py --sys_name couette --train True \
     --n_hidden 2 --dim_hidden 128 --passes 10 \
     --miles 500 1500 --gamma 0.1 --max_epoch 2000 \
-    --noise_var 4e-4 --lr 1e-4 --lambda_d 10 \
-    --gpu False
+    --noise_var 4e-4 --lr 1e-4 --lambda_d 10
