0ffe7b51ad4fdb6960f0d1df69dfbd85848bb2b7
Quercus <63294776+quercushernandez@users.noreply.github.com>
2024-03-12 09:53:55 -0400

    Update run_cylinder_train.sh

--- executables/run_cylinder_train.sh
+++ executables/run_cylinder_train.sh
@@ -2,4 +2,5 @@
 python main.py --sys_name cylinder --train True \
     --n_hidden 2 --dim_hidden 128 --passes 10 \
     --miles 500 1500 --gamma 0.1 --max_epoch 2000 \
-    --noise_var 4e-4 --lr 1e-4 --lambda_d 10
+    --noise_var 4e-4 --lr 1e-4 --lambda_d 10 \
+    --batch_size 4
